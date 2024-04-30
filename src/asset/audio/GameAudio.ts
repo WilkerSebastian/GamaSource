7c0e339e932e4c8ba38ebbe3bb48541485b7ab88
WilkerSebastian <wilkerafonso2005@gmail.com>
2024-04-30 00:27:27 -0400

    up: GameAudio methods

--- src/asset/audio/GameAudio.ts
+++ src/asset/audio/GameAudio.ts
@@ -1,92 +1,82 @@
-import GamaSource from "../../GamaSource"
+import GamaSource from "../../GamaSource";
 
 export default class GameAudio {
 
-    private source:HTMLAudioElement
+    private source: HTMLAudioElement;
 
-    constructor(path:string) {
+    constructor(path: string) {
+        this.source = new Audio();
 
-        this.source = new Audio()
-        
         this.source.addEventListener("loadeddata", () => {
+            GamaSource.LOAD++;
+        });
 
-            GamaSource.LOAD++
+        this.source.src = path;
+        this.setVolume(50);
 
-        }) 
-
-        this.source.src = path
-
-        this.setVolume(50)
-
-        const name = path.split("/")
-
-        GamaSource.ASSETS.set(name[name.length - 1], this)
-
+        const name = path.split("/");
+        GamaSource.ASSETS.set(name[name.length - 1], this);
     }
 
     public pause() {
-
-        this.source.pause()
-
+        this.source.pause();
     }
 
-    public setLoop(loop:boolean) {
-
-        this.source.loop = loop 
-
+    public setLoop(loop: boolean) {
+        this.source.loop = loop;
     }
 
-    public async playTo(start:number, end?:number) {
+    public async playTo(start: number, end?: number) {
+        const maxTime = end ?? this.getDuration();
 
-        const max_time = end ?? this.getDuration();
-
-        this.source.currentTime = start
+        this.source.currentTime = start;
 
         this.source.addEventListener("timeupdate", () => {
+            if (this.source.currentTime >= maxTime)
+                this.pause();
+        });
 
-            if (this.source.currentTime >= max_time)
-                this.pause()
-
-        })
-
-        await this.play()
-
+        await this.play();
     }
 
     public getDuration() {
-
-        return this.source.duration
-
+        return this.source.duration;
     }
 
-    public setVolume(volume:number) { 
-
-        this.source.volume = volume / 100
-
-    } 
+    public setVolume(volume: number) {
+        this.source.volume = volume / 100;
+    }
 
     public getVolume() {
-
-        return this.source.volume
-
+        return this.source.volume;
     }
 
-    public setAutoPlay(auto:boolean) {
-
-        this.source.autoplay = auto
-
+    public setAutoPlay(auto: boolean) {
+        this.source.autoplay = auto;
     }
 
     public async play() {
+        await this.source.play();
+    }
 
-        await this.source.play()
+    public stop() {
+        this.source.pause();
+        this.source.currentTime = 0;
+    }
 
+    public seek(time: number) {
+        this.source.currentTime = time;
+    }
+
+    public onEnded(callback: () => void) {
+        this.source.addEventListener("ended", callback);
+    }
+
+    public isPlaying(): boolean {
+        return !this.source.paused;
     }
 
     public getSource() {
-
-        return this.source
-
+        return this.source;
     }
-
 }
