97ce3a11a68653facc6e643033725b8181d0bd10
WilkerSebastian <wilkerafonso2005@gmail.com>
2024-04-30 10:33:48 -0400

    fix: random integer out of range

--- src/math/GameMath.ts
+++ src/math/GameMath.ts
@@ -22,7 +22,9 @@ export default abstract class GameMath {
      * @returns A random integer between min (inclusive) and max (exclusive).
      */
     public static randomInteger(min: number = 0, max: number = 1): number {
-        return this.parseInt(this.random(max - min + 1));
+        min = Math.ceil(min);
+        max = Math.floor(max);
+        return Math.floor(Math.random() * (max - min + 1)) + min;
     }
 
     /**
