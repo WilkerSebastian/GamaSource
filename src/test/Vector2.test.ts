be5e3766b89d4e179ec9aa046bb802123d83800c
WilkerSebastian <wilkerafonso2005@gmail.com>
2024-05-28 10:49:38 -0400

    add: init test

---
+++ src/test/Vector2.test.ts
@@ -0,0 +1,41 @@
+import { Vector2 } from "../GamaSource";
+
+describe("instance vector", () => {
+
+    it("new vector2", () => {
+
+        const vec = new Vector2(5 , -7)
+
+        expect([
+            vec.x,
+            vec.y
+        ]).toEqual([5, -7])
+
+    })
+
+    it("vector2 zero", () => {
+
+        const vec = Vector2.zero()
+
+        expect([
+            vec.x,
+            vec.y
+        ]).toEqual([0, 0])
+
+    })
+
+    it("instance result of operation", () => {
+
+        const vec = new Vector2(-3 , 5).add(new Vector2(2, 1))
+
+        expect([
+            vec.x,
+            vec.y
+        ]).toEqual([
+            -1,
+            6
+        ])
+
+    })
+
+})
