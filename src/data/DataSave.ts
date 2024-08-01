6a4653d94ef256f6ea6504fa2727b24dd67960ac
WilkerSebastian <wilkerafonso2005@gmail.com>
2024-08-01 00:14:52 -0400

    add: DataSave class to maneger localstorage

---
+++ src/data/DataSave.ts
@@ -0,0 +1,60 @@
+import { GameObject } from "../GamaSource";
+
+export default class DataSave<T extends GameObject> {
+
+    private storageKey: string
+
+    constructor(storageKey:string) {
+
+        this.storageKey = storageKey
+
+    }
+
+    public save(item: T): void {
+
+        const items = this.getAll();
+        items.push(item);
+        localStorage.setItem(this.storageKey, JSON.stringify(items));
+
+    }
+
+    public getAll(): T[] {
+
+        const data = localStorage.getItem(this.storageKey);
+        return data ? JSON.parse(data) : [];
+
+    }
+
+    public getById(tag: string): T | undefined {
+    
+        const items = this.getAll();
+        return items.find(item => item.tag === tag);
+    
+    }
+
+    public update(item: T): void {
+    
+        const items = this.getAll();
+        const index = items.findIndex(existingItem => existingItem.tag === item.tag);
+
+        if (index !== -1) {
+            items[index] = item;
+            localStorage.setItem(this.storageKey, JSON.stringify(items));
+        }
+    
+    }
+
+    public delete(tag: string): void {
+    
+        const items = this.getAll();
+        const filteredItems = items.filter(item => item.tag !== tag);
+        localStorage.setItem(this.storageKey, JSON.stringify(filteredItems));
+    
+    }
+
+    public clear(): void {
+        
+        localStorage.removeItem(this.storageKey);
+
+    }
+}
